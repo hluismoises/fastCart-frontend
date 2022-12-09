ef071f5748a5a404ebe3fc604b6c4d8c93fa0683
hluismoises <hluismoises@gmail.com>
2022-12-09 02:11:07 -0600

    Login page con funcionalidad terminada

--- src/app/components/pages/login/login.component.ts
+++ src/app/components/pages/login/login.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-login',
@@ -9,14 +11,22 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 export class LoginComponent implements OnInit {
   loginForm!: FormGroup;
   isSubmitted = false;
-
-  constructor(private formBuilder: FormBuilder) {}
+  returnUrl = '';
+  message = '';
+  constructor(
+    private formBuilder: FormBuilder,
+    private userService: UserService,
+    private activatedRoute: ActivatedRoute,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
     this.loginForm = this.formBuilder.group({
       email: ['', [Validators.required, Validators.email]],
       password: ['', Validators.required],
     });
+
+    this.returnUrl = this.activatedRoute.snapshot.queryParams.returnUrl;
   }
 
   //Obtener el FormControl como una propiedad para utilizarlo en el HTML
@@ -27,13 +37,17 @@ export class LoginComponent implements OnInit {
 
   submit() {
     this.isSubmitted = true;
-    if (this.loginForm.invalid) return;
+    if (this.loginForm.invalid) {
+      this.message = 'Email o password incorrecto';
+      return;
+    }
 
-    alert(`email: ${this.fc.email.value}, password: ${this.fc.password.value}`);
+    //alert(`email: ${this.fc.email.value}, password: ${this.fc.password.value}`);
 
-    /* this.userService.login({email:this.fc.email.value,
-       password: this.fc.password.value}).subscribe(() => {
-         this.router.navigateByUrl(this.returnUrl);
-       }); */
+    this.userService
+      .login({ email: this.fc.email.value, password: this.fc.password.value })
+      .subscribe(() => {
+        this.router.navigateByUrl(this.returnUrl);
+      });
   }
 }
--- src/app/shared/constants/url.ts
+++ src/app/shared/constants/url.ts
@@ -1,8 +1,12 @@
 const BASE_URL = 'http://localhost:3000';
 
+//Api productos
 export const PRODUCTOS_URL = BASE_URL + '/api/productos';
 export const PRODUCTOS_LISTAEMPRESAS_URL =
   'http://localhost:3000/api/productos/empresas';
 export const PRODUCTOS_BY_SEARCH_URL = PRODUCTOS_URL + '/search/';
 export const PRODUCTOS_BY_EMPRESA_URL = PRODUCTOS_URL + '/empresa/';
 export const PRODUCTO_BY_ID_URL = PRODUCTOS_URL + '/';
+
+//Api usuarios
+export const USER_LOGIN_URL = BASE_URL + '/api/users/login';
